a70aaf48595478131580391b7f4b9b75fda3de1f
CITE243Python <tlhenderson2@nic.edu>
2025-08-27 13:24:33 -0700

    Delete myPets.py

--- myPets.py
+++
@@ -1,7 +0,0 @@
-my_pets = ['Zophie', 'Pooka', 'Fat-tail']
-print('Enter a pet name:')
-name = input()
-if name not in my_pets:
-    print('I do not have a pet named ' + name)
-else:
-    print(name + ' is my pet.')
