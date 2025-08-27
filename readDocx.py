840e269012f870d9817faa407ae55df9b14e8280
CITE243Python <tlhenderson2@nic.edu>
2025-08-27 13:27:17 -0700

    Delete readDocx.py

--- readDocx.py
+++
@@ -1,8 +0,0 @@
-import docx
-
-def get_text(filename):
-    doc = docx.Document(filename)
-    full_text = []
-    for para in doc.paragraphs:
-        full_text.append(para.text)
-    return '\n\n'.join(full_text)
